221c25a59c685a565fc7762cb452df0df33a3bd8
ovarz <ovarz_03@yahoo.com>
2025-11-25 13:59:55 +0700

    a

--- template/inc/base.php
+++
@@ -1,8 +0,0 @@
-<!DOCTYPE html>
-<html lang="id">
-<base href="/" />
-<?php 
-  $_SERVER['BMG'] = $_SERVER['DOCUMENT_ROOT'] . '/';  
-  $sitename = 'Borussia Mönchengladbach Academy Indonesia';
-  $anticache = date ('s'.'i'.'H'.'d'.'m'.'Y');
-?>
--- template/inc/footer.php
+++
@@ -1,5 +0,0 @@
-<footer class="content-center">
-  <div class="site-container">
-    <div class="footer-copyright content-center"><?php echo $sitename; ?> &copy;2025 - All Right Reserved</div>
-  </div>
-</footer>
