b7a6f1015d0d2194f995baa9c3a689c07cb41677
ovarz <ovarz_03@yahoo.com>
2026-01-31 19:52:39 +0700

    a

--- coach/absence/index.php
+++ coach/absence/index.php
@@ -17,7 +17,7 @@
 		  <div class="jotform">
 		    <style>
 			  body::-webkit-scrollbar{width:1px;}
-			  .shortcut-box .jotform:after{background-color:#f3f3fe;}
+			  .rancak-foundation, .shortcut-box .jotform:after{background-color:#f3f3fe;}
 			</style>
 			<iframe
 			  id="JotFormIFrame-260290932274053"
